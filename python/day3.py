803f4b472a2fbfdb60a885dbd06ac73729b541f0
DeShrike <deshrike@hotmail.com>
2023-12-03 10:11:09 +0100

    Day 3 done

--- python/day3.py
+++ python/day3.py
@@ -55,15 +55,8 @@ class Day3Solution(Aoc):
 
     def TestDataB(self):
         self.inputdata.clear()
-        # self.TestDataA()    # If test data is same as test data for part A
-        testdata = \
-        """
-        1000
-        2000
-        3000
-        """
-        self.inputdata = [line.strip() for line in testdata.strip().split("\n")]
-        return None
+        self.TestDataA()
+        return 467835
 
     def Neighbours(self, x: int, y: int, l: int, w: int, h: int):
         for yy in dirange(y - 1, y + 1):
@@ -73,6 +66,7 @@ class Day3Solution(Aoc):
 
     def ParseInput(self):
         numbers = []
+        gears = {}
         w = len(self.inputdata[0])
         h = len(self.inputdata)
         n = None
@@ -91,13 +85,18 @@ class Day3Solution(Aoc):
             for nx, ny in self.Neighbours(num.x, num.y, len(num.number), w, h):
                 if self.inputdata[ny][nx] != "." and not self.inputdata[ny][nx].isdigit():
                     num.symbols.append(self.inputdata[ny][nx])
+                    if self.inputdata[ny][nx] == "*":
+                        if (nx, ny) in gears:
+                            gears[(nx, ny)].append(int(num.number))
+                        else:
+                            gears[(nx, ny)] = [int(num.number)]
 
-        return numbers
+        return numbers, gears
 
     def PartA(self):
         self.StartPartA()
 
-        numbers = self.ParseInput()
+        numbers, _ = self.ParseInput()
         answer = sum([int(n.number) for n in numbers if len(n.symbols) > 0])
 
         self.ShowAnswer(answer)
@@ -105,9 +104,8 @@ class Day3Solution(Aoc):
     def PartB(self):
         self.StartPartB()
 
-        # Add solution here
-
-        answer = None
+        _, gears = self.ParseInput()
+        answer = sum([g[0] * g[1] for g in gears.values() if len(g) == 2])
 
         self.ShowAnswer(answer)
 
