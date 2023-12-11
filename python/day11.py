d1f124a163cd5e2e60ddaf6651367ad20d63c2c5
DeShrike <deshrike@hotmail.com>
2023-12-11 10:41:53 +0100

    Day 11 done

--- python/day11.py
+++ python/day11.py
@@ -11,6 +11,7 @@ class Day11Solution(Aoc):
     def Run(self):
         self.StartDay(11, "Cosmic Expansion")
         self.ReadInput()
+        self.expand_by = 1_000_000
         self.PartA()
         self.PartB()
 
@@ -45,15 +46,9 @@ class Day11Solution(Aoc):
 
     def TestDataB(self):
         self.inputdata.clear()
-        # self.TestDataA()    # If test data is same as test data for part A
-        testdata = \
-        """
-        1000
-        2000
-        3000
-        """
-        self.inputdata = [line.strip() for line in testdata.strip().split("\n")]
-        return None
+        self.expand_by = 100
+        self.TestDataA()
+        return 8410
 
     def ParseInput(self):
         grid = []
@@ -63,28 +58,51 @@ class Day11Solution(Aoc):
 
     def Expand(self, grid):
         hgrid = []
+        r = c = 0
         for line in grid:
             hgrid.append(line)
             if all([c == "." for c in line]):
+                r = r + 1
                 hgrid.append(line[:])
 
         for x in dirange(len(hgrid[0]) - 1, 0):
             if all([row[x] == "." for row in hgrid]):
+                c = c + 1
                 for row in hgrid:
                     row.insert(x, ".")
+        print(f"Extra rows: {r}  Extra columns: {c}")
         return hgrid
+
+    def FindEmpty(self, grid):
+        erows = []
+        ecols = []
+        for y, line in enumerate(grid):
+            if all([c == "." for c in line]):
+                erows.append(y)
+
+        for x in range(len(grid[0])):
+            if all([row[x] == "." for row in grid]):
+                ecols.append(x)
+        return erows, ecols
+
+    def isbetween(self, v: int, a: int, b: int) -> bool:
+        if a > b: a, b = b, a
+        return a < v < b
+    
+    def FindGalaxies(self, grid):
+        galaxies = []
+        for y, row in enumerate(grid):
+            for x, c in enumerate(row):
+                if c == "#":
+                    galaxies.append((x, y))
+        return galaxies
 
     def PartA(self):
         self.StartPartA()
 
         universe = self.ParseInput()
         universe = self.Expand(universe)
-
-        galaxies = []
-        for y, row in enumerate(universe):
-            for x, c in enumerate(row):
-                if c == "#":
-                    galaxies.append((x, y))
+        galaxies = self.FindGalaxies(universe)
 
         answer = 0
         for c in itertools.combinations(galaxies, 2):
@@ -95,9 +113,26 @@ class Day11Solution(Aoc):
     def PartB(self):
         self.StartPartB()
 
-        # Add solution here
+        universe = self.ParseInput()
+        erows, ecols = self.FindEmpty(universe)
+        galaxies = self.FindGalaxies(universe)
+        print(ecols)
+        print(erows)
+        # print(galaxies)
 
-        answer = None
+        answer = 0
+        for c in itertools.combinations(galaxies, 2):
+            dist = manhattan_distance(c[0], c[1])
+            er = len([v for v in erows if self.isbetween(v, c[0][1], c[1][1])])
+            ec = len([v for v in ecols if self.isbetween(v, c[0][0], c[1][0])])
+
+            # print(ecols)
+            # print(erows)
+            # print(f" X: {c[0][0]} -> {c[1][0]}    Y: {c[0][1]} -> {c[1][1]} ")
+            # print("ec", ec)
+            # print("er", er)
+            # a = input()
+            answer += dist + er * (self.expand_by - 1) + ec * (self.expand_by - 1)
 
         self.ShowAnswer(answer)
 
